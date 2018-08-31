2f646b90f30af92b5740b2a5943a95d42b54dcac
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-08-31 08:09:48 +0500

    Update Added New Js File

---
+++ Asp.AngularCore.git/wwwroot/Js/ProjectScript.js
@@ -0,0 +1,19 @@
+﻿//var s = "Usman Haider Khan";
+
+//alert("Hello Usman");
+
+console.log("Hello Khan");
+var formdata = document.getElementById("theForm");
+formdata.hidden = true;
+
+
+var shopbtn = document.getElementById("buybtn");
+shopbtn.addEventListener("click", function () {
+    alert("You Buyed the Product Successfully");
+    //  console.log("You Buyed the Product Successfully");
+
+});
+
+
+var product_Info = document.getElementsByClassName("product-info");
+var listItem = product_Info.item[0].children;
