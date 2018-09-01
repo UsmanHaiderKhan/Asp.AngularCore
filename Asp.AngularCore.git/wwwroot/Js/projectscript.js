e81675e9a71dbc39a8cd05f8ab0ad066416baf4c
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-09-01 11:39:53 +0500

    Update the Convert the Javascript Code in to the Jquery

--- Asp.AngularCore.git/wwwroot/Js/projectscript.js
+++ Asp.AngularCore.git/wwwroot/Js/projectscript.js
@@ -1,15 +1,34 @@
 ﻿console.log("Hello Khan");
-var formdata = document.getElementById("theForm");
-formdata.hidden = true;
+//var formdata = document.getElementById("theForm");
+//formdata.hidden = true;
+
+var theForm = $("#theForm");
+theForm.hide();
 
 
-var shopbtn = document.getElementById("buybtn");
-shopbtn.addEventListener("click", function () {
-    alert("You Buyed the Product Successfully");
-    //  console.log("You Buyed the Product Successfully");
+//var shopbtn = document.getElementById("buybtn");
+//shopbtn.addEventListener("click", function () {
+//    alert("You Buyed the Product Successfully");
+//    console.log("You Buyed the Product Successfully");
 
-});
+//});
 
 
-var product_Info = document.getElementsByClassName("product-info");
-var listItem = product_Info.item[0].children;
+var shopBtn = $("#buybtn");
+shopBtn.on("click",
+    function () {
+        console.log("You Buyed the Product Successfully");
+    });
+
+
+
+//var product_Info = document.getElementsByClassName("product-info");
+//var listItem = product_Info.item[0].children;
+
+
+var product_Info = $(".product-info li");
+product_Info.on("click",
+    function () {
+        // console.log("You Clicked On Me...!" + this.innerText);
+        console.log("You Clicked On Me...!" + $(this).text());
+    });
