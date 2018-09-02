47fdf4c7406fa3044bb91699c2ff4699caf2ec7b
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-09-02 08:31:35 +0500

    Added New Controller App

---
+++ Asp.AngularCore.git/Controller/AppController.cs
@@ -0,0 +1,37 @@
+﻿using Asp.AngularCore.git.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.AngularCore.git.Controller
+{
+    public class AppController : Microsoft.AspNetCore.Mvc.Controller
+    {
+
+        public IActionResult Index()
+        {
+
+            return View();
+
+        }
+        [HttpGet]
+        public IActionResult Contact()
+        {
+            ViewBag.Title = "Contact Us";
+            //   throw new InvalidOperationException("Some thing happen bad");
+            return View();
+
+        }
+        [HttpPost]
+        public IActionResult Contact(Contact contact)
+        {
+            return View();
+
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+
+
+    }
+
+}
