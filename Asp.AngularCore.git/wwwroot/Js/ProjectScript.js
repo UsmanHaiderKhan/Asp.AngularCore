ef233f8cba39b765d184774e6fe7ccd46d9b6a37
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-09-02 08:44:20 +0500

    Update the JavaScript File

--- Asp.AngularCore.git/wwwroot/Js/ProjectScript.js
+++ Asp.AngularCore.git/wwwroot/Js/ProjectScript.js
@@ -1,35 +1,44 @@
-﻿
-console.log("Hello Khan");
-var formdata = document.getElementById("theForm");
-formdata.hidden = true;
-
-//var theForm = $("#theForm");
-//theForm.hide();
+﻿/// <reference path="../mdb-free/js/jquery-3.3.1.min.js" />
+$(document).ready(function () {
 
 
-//var shopbtn = document.getElementById("buybtn");
-//shopbtn.addEventListener("click", function () {
-//    alert("You Buyed the Product Successfully");
-//    console.log("You Buyed the Product Successfully");
+    console.log("Hello Khan");
+    //var formdata = document.getElementById("theForm");
+    //formdata.hidden = true;
 
-//});
-
-
-var shopBtn = $("#buybtn");
-shopBtn.on("click",
-    function () {
-        console.log("You Buyed the Product Successfully");
-    });
+    var theForm = $("#theForm");
+    theForm.hide();
 
 
+    //var shopbtn = document.getElementById("buybtn");
+    //shopbtn.addEventListener("click", function () {
+    //    alert("You Buyed the Product Successfully");
+    //    console.log("You Buyed the Product Successfully");
 
-//var product_Info = document.getElementsByClassName("product-info");
-//var listItem = product_Info.item[0].children;
+    //});
 
 
-var product_Info = $(".product-info li");
-product_Info.on("click",
-    function () {
-        // console.log("You Clicked On Me...!" + this.innerText);
-        console.log("You Clicked On Me...!" + $(this).text());
-    });
+    var shopBtn = $("#buybtn");
+    shopBtn.on("click",
+        function () {
+            console.log("You Buyed the Product Successfully");
+        });
+
+
+    //var product_Info = document.getElementsByClassName("product-info");
+    //var listItem = product_Info.item[0].children;
+
+
+    var productInfo = $(".product-info li");
+    productInfo.on("click",
+        function () {
+            // console.log("You Clicked On Me...!" + this.innerText);
+            console.log("Your Buying Items:" + $(this).text());
+        });
+    var $loginToggle = $("#Toggling");
+    var $loginForm = $("#loginForm");
+    $loginToggle.on("click",
+        function () {
+            $loginForm.slideToggle(1000);
+        });
+});
