f456a2e6d44ec790c4df56b5574cb6ceae4da766
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-09-02 08:39:42 +0500

    Update Added Contact class for the Model

---
+++ Asp.AngularCore.git/ViewModel/Contact.cs
@@ -0,0 +1,12 @@
+﻿namespace Asp.AngularCore.git.ViewModel
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public long Phone { get; set; }
+        public string Email { get; set; }
+        public string Subject { get; set; }
+        public string Message { get; set; }
+    }
+}
