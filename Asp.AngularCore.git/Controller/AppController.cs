8be1363c4bfee8de2c53cefe8c191a44f9fb2b12
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-09-03 17:42:47 +0500

    Update the App Controller with the action method of the shop

--- Asp.AngularCore.git/Controller/AppController.cs
+++ Asp.AngularCore.git/Controller/AppController.cs
@@ -1,10 +1,23 @@
-﻿using Asp.AngularCore.git.ViewModel;
+﻿using Asp.AngularCore.git.Data;
+using Asp.AngularCore.git.Services;
+using Asp.AngularCore.git.ViewModel;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace Asp.AngularCore.git.Controller
 {
     public class AppController : Microsoft.AspNetCore.Mvc.Controller
     {
+
+        private readonly IMailService _mailService;
+        private readonly LKContext _db;
+
+        public AppController(IMailService mailService, LKContext db)
+        {
+            _mailService = mailService;
+            _db = db;
+        }
+
 
         public IActionResult Index()
         {
@@ -23,6 +36,17 @@ namespace Asp.AngularCore.git.Controller
         [HttpPost]
         public IActionResult Contact(Contact contact)
         {
+            if (ModelState.IsValid)
+            {
+                _mailService.Message("usmanhaiderkhan4@gmail.com", contact.Subject, $"From:{contact.Email} - {contact.Name} - {contact.Phone} Message:{contact.Message}");
+                ViewBag.Message = "Sent the Email successfully";
+                ModelState.Clear();
+                //send Email
+            }
+            else
+            {
+                //error
+            }
             return View();
 
         }
@@ -31,6 +55,13 @@ namespace Asp.AngularCore.git.Controller
             return View();
         }
 
+        public IActionResult Shop()
+        {
+            var result = from c in _db.Products orderby c.Category select c;
+
+            return View(result.ToList());
+
+        }
 
     }
 
