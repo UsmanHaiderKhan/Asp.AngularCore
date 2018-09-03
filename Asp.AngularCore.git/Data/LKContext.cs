823e76ef408e72737af28ea6500f027616b53f78
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-09-03 17:44:08 +0500

    Update Add New Context Class

---
+++ Asp.AngularCore.git/Data/LKContext.cs
@@ -0,0 +1,17 @@
+﻿using Asp.AngularCore.git.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Asp.AngularCore.git.Data
+{
+    public class LKContext : DbContext
+    {
+        public LKContext(DbContextOptions<LKContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+    }
+
+}
