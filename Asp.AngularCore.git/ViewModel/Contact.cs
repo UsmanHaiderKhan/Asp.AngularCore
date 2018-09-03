07943f01cb03c8651d8d3bfe5d1534dac4c8b936
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-09-03 17:46:18 +0500

    Update te class contact

--- Asp.AngularCore.git/ViewModel/Contact.cs
+++ Asp.AngularCore.git/ViewModel/Contact.cs
@@ -1,12 +1,23 @@
-﻿namespace Asp.AngularCore.git.ViewModel
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Asp.AngularCore.git.ViewModel
 {
     public class Contact
     {
         public int Id { get; set; }
+        [Required]
+        [MinLength(5)]
         public string Name { get; set; }
+        [Required]
         public long Phone { get; set; }
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+        [Required]
+        [MinLength(6)]
         public string Subject { get; set; }
+        [Required]
+        [MaxLength(250)]
         public string Message { get; set; }
     }
 }
