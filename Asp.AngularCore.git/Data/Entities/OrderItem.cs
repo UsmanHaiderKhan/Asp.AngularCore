7533acabba1358e19a99d383e58dfd5051ced776
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-09-03 17:43:30 +0500

    Added the new entities class OrderItem

---
+++ Asp.AngularCore.git/Data/Entities/OrderItem.cs
@@ -0,0 +1,11 @@
+﻿namespace Asp.AngularCore.git.Data.Entities
+{
+    public class OrderItem
+    {
+        public int Id { get; set; }
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+        public Order Order { get; set; }
+        public Product Product { get; set; }
+    }
+}
