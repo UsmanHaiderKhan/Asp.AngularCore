f87252e17a88f16e4e9feb7d83023a4635bda1b5
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-09-03 17:46:01 +0500

    Added New class nullemail

---
+++ Asp.AngularCore.git/Services/NullEmail.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Asp.AngularCore.git.Services
+{
+    public class NullEmail : IMailService
+    {
+        private readonly ILogger<NullEmail> _logger;
+
+        public NullEmail(ILogger<NullEmail> logger)
+        {
+            _logger = logger;
+        }
+        public void Message(string to, string subject, string body)
+        {
+            _logger.LogInformation($"To:{to}  Subject:{subject}   Body:{body}");
+        }
+    }
+}
