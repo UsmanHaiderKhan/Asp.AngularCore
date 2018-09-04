58a07272da943e60bf17f9b3041f8b3d9ed41a58
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-09-04 16:39:37 +0500

    Added New class for the repository

---
+++ Asp.AngularCore.git/Data/LKRepository.cs
@@ -0,0 +1,46 @@
+﻿using Asp.AngularCore.git.Data.Entities;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Asp.AngularCore.git.Data
+{
+    public class LKRepository : ILKRepository
+    {
+        private readonly LKContext _context;
+
+        public LKRepository(LKContext context)
+        {
+            _context = context;
+        }
+
+        public List<Product> GetAllProducts()
+        {
+            using (_context)
+            {
+                return (from c in _context.Products
+                    .OrderBy(m => m.Title)
+                        select c).ToList();
+            }
+        }
+
+        public List<Product> GetProductsByCategory(string category)
+        {
+            using (_context)
+            {
+                return (from c in _context.Products
+                    .Where(c => c.Category == category)
+                        select c).ToList();
+            }
+        }
+
+        public bool SaveAll()
+        {
+            return _context.SaveChanges() > 0;
+        }
+
+        public bool SaveChanges()
+        {
+            return _context.SaveChanges() > 0;
+        }
+    }
+}
