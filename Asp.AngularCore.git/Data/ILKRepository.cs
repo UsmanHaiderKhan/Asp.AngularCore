c5f953c7c3a91a729059b4666b4a59e6701a4aa5
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-09-04 16:39:18 +0500

    Added New interface ILkRepository

---
+++ Asp.AngularCore.git/Data/ILKRepository.cs
@@ -0,0 +1,12 @@
+﻿using Asp.AngularCore.git.Data.Entities;
+using System.Collections.Generic;
+
+namespace Asp.AngularCore.git.Data
+{
+    public interface ILKRepository
+    {
+        List<Product> GetAllProducts();
+        List<Product> GetProductsByCategory(string category);
+        bool SaveChanges();
+    }
+}
