18c4a49ef03c7c927f95b2902617cfae501bb344
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-09-05 16:02:10 +0500

    Update the Order Controller Added the BadRequest Query

---
+++ Asp.AngularCore.git/Controller/OrdersController.cs
@@ -0,0 +1,35 @@
+﻿using Asp.AngularCore.git.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace Asp.AngularCore.git.Controller
+{
+    [Route("api/[Controller]")]
+    public class OrdersController : Microsoft.AspNetCore.Mvc.Controller
+    {
+        private readonly ILKRepository _repository;
+        private readonly ILogger<LKRepository> _logger;
+
+        public OrdersController(ILKRepository repository, ILogger<LKRepository> logger)
+        {
+            _repository = repository;
+            _logger = logger;
+        }
+        [HttpGet]
+        public IActionResult GetOrder()
+        {
+            try
+            {
+                return Ok(_repository.GetAllOrders());
+            }
+            catch (Exception e)
+            {
+                _logger.LogError($"Failed To Get All Products:{e}");
+                return BadRequest("Failed to Get the Orders");
+            }
+
+        }
+    }
+}
