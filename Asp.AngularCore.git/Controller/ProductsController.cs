b2d63f91362d2476001ad460c29d9d889d6fbbb4
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-09-05 15:54:15 +0500

    Added New Controller To Get All the Products through APIs

---
+++ Asp.AngularCore.git/Controller/ProductsController.cs
@@ -0,0 +1,36 @@
+﻿using Asp.AngularCore.git.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace Asp.AngularCore.git.Controller
+{
+    [Route("api/[Controller]")]
+    public class ProductsController : Microsoft.AspNetCore.Mvc.Controller
+    {
+        private readonly ILKRepository _repository;
+        private readonly ILogger<LKRepository> _logger;
+
+        public ProductsController(ILKRepository repository, ILogger<LKRepository> logger)
+        {
+            _repository = repository;
+            _logger = logger;
+        }
+
+        public IActionResult Get()
+        {
+            try
+            {
+                return Ok(_repository.GetAllProducts());
+            }
+            catch (Exception e)
+            {
+                _logger.LogError($"Failed to Get the Products:{e}");
+                return BadRequest("Failed ");
+            }
+
+        }
+
+    }
+}
