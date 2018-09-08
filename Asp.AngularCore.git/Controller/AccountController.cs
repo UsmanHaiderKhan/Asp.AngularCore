f9153ae2b4a65f4c66bda6c7f944a72d11f63070
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-09-08 13:00:45 +0500

    Added New Controller Account to Handle the Users

---
+++ Asp.AngularCore.git/Controller/AccountController.cs
@@ -0,0 +1,63 @@
+﻿using Asp.AngularCore.git.Data.Entities;
+using Asp.AngularCore.git.ViewModel;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp.AngularCore.git.Controller
+{
+    public class AccountController : Microsoft.AspNetCore.Mvc.Controller
+    {
+        private readonly ILogger<AccountController> _logger;
+        private readonly SignInManager<StoreUser> _signInManager;
+
+        public AccountController(ILogger<AccountController> logger, SignInManager<StoreUser> signInManager)
+        {
+            _logger = logger;
+            _signInManager = signInManager;
+        }
+
+        public IActionResult Login()
+        {
+            if (this.User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Index", "App");
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await
+                    _signInManager.PasswordSignInAsync(model.UserName,
+                        model.Password,
+                        model.RememberMe, false);
+                if (result.Succeeded)
+                {
+                    if (Request.Query.Keys.Contains("ReturnUrl"))
+                    {
+                        Redirect(Request.Query["ReturnUrl"].First());
+                    }
+                    else
+                    {
+                        return RedirectToAction("Shop", "App");
+                    }
+                }
+            }
+            ModelState.AddModelError("", "Failed To Login");
+            return View();
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "App");
+
+        }
+    }
+}
