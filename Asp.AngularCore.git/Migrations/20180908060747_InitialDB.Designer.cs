5bb0ac5864e28c08d37288ce8903a9c9a89718ec
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-09-08 13:02:08 +0500

    Update the Migration Delete the Whole Database Create Again through migration

--- Asp.AngularCore.git/Migrations/20180908060747_InitialDB.Designer.cs
+++ Asp.AngularCore.git/Migrations/20180908060747_InitialDB.Designer.cs
@@ -4,14 +4,16 @@ using Asp.AngularCore.git.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Asp.AngularCore.git.Migrations
 {
     [DbContext(typeof(LKContext))]
-    partial class LKContextModelSnapshot : ModelSnapshot
+    [Migration("20180908060747_InitialDB")]
+    partial class InitialDB
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
