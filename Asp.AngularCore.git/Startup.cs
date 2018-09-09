4ce2528d581a1b0b8ddf97befb04bb9f43babc7d
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-09-09 09:21:45 +0500

    Update the Start up view To Write the Token

--- Asp.AngularCore.git/Startup.cs
+++ Asp.AngularCore.git/Startup.cs
@@ -1,35 +1,61 @@
 ﻿using Asp.AngularCore.git.Data;
+using Asp.AngularCore.git.Data.Entities;
 using Asp.AngularCore.git.Services;
 using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 using Newtonsoft.Json;
+using System.Text;
 
 namespace Asp.AngularCore.git
 {
     public class Startup
     {
         private readonly IConfiguration _configuration;
-        public Startup(IConfiguration configuration)
+        private readonly IHostingEnvironment _env;
+
+        public Startup(IConfiguration configuration, IHostingEnvironment env)
         {
             _configuration = configuration;
+            _env = env;
         }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddIdentity<StoreUser, IdentityRole>(cfg => { cfg.User.RequireUniqueEmail = true; })
+                .AddEntityFrameworkStores<LKContext>();
+
+            services.AddAuthentication().AddCookie()
+                .AddJwtBearer(cfg =>
+                {
+                    cfg.TokenValidationParameters = new TokenValidationParameters()
+                    {
+                        ValidIssuer = _configuration["Tokens:Issuer"],
+                        ValidAudience = _configuration["Tokens:Audience"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Tokens:Key"]))
+                    };
+                });
+
             services.AddDbContext<LKContext>(cfg => { cfg.UseSqlServer(_configuration.GetConnectionString("LKConnectionString")); });
             services.AddTransient<IMailService, NullEmail>();
             services.AddTransient<LkSeeds>();
-
             services.AddAutoMapper();
-
             services.AddScoped<ILKRepository, LKRepository>();
-            services.AddMvc().AddJsonOptions(opt => opt.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore);
+            services.AddMvc(opt =>
+            {
+                if (_env.IsProduction())
+                {
+                    opt.Filters.Add(new RequireHttpsAttribute());
+                }
+            }).AddJsonOptions(opt => opt.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,17 +69,13 @@ namespace Asp.AngularCore.git
             {
                 app.UseExceptionHandler("/error");
             }
-
             app.UseStaticFiles();
-
+            app.UseAuthentication();
             app.UseMvc(cfg =>
             {
                 cfg.MapRoute("Default", "/{controller}/{action}/{id?}",
                     new { Controller = "App", Action = "Index" });
             });
-
-
-
             app.Run(async (context) =>
             {
                 await context.Response.WriteAsync("Hello World!");
@@ -64,7 +86,7 @@ namespace Asp.AngularCore.git
                 using (var scope = app.ApplicationServices.CreateScope())
                 {
                     var seeder = scope.ServiceProvider.GetService<LkSeeds>();
-                    seeder.Seed();
+                    seeder.Seed().Wait();
                 }
             }
         }
