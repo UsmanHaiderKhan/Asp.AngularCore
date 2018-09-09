cde1692a0c6ce4abf9ee1c52930e475c23cd91d4
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-09-09 09:12:25 +0500

    Update the Account Controller Added a Action to create a Token

--- Asp.AngularCore.git/Controller/AccountController.cs
+++ Asp.AngularCore.git/Controller/AccountController.cs
@@ -2,8 +2,14 @@
 using Asp.AngularCore.git.ViewModel;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace Asp.AngularCore.git.Controller
@@ -12,11 +18,18 @@ namespace Asp.AngularCore.git.Controller
     {
         private readonly ILogger<AccountController> _logger;
         private readonly SignInManager<StoreUser> _signInManager;
+        private readonly UserManager<StoreUser> _userManager;
+        private readonly IConfiguration _config;
 
-        public AccountController(ILogger<AccountController> logger, SignInManager<StoreUser> signInManager)
+        public AccountController(ILogger<AccountController> logger,
+            SignInManager<StoreUser> signInManager,
+            UserManager<StoreUser> userManager,
+            IConfiguration config)
         {
             _logger = logger;
             _signInManager = signInManager;
+            _userManager = userManager;
+            _config = config;
         }
 
         public IActionResult Login()
@@ -58,6 +71,46 @@ namespace Asp.AngularCore.git.Controller
             await _signInManager.SignOutAsync();
             return RedirectToAction("Index", "App");
 
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateToken([FromBody]LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByNameAsync(model.UserName);
+                var result = await _signInManager.CheckPasswordSignInAsync(user, model.Password, false);
+                if (result.Succeeded)
+                {
+                    //create the Token
+                    var claim = new[]
+                    {
+                        new Claim(JwtRegisteredClaimNames.Sub,user.Email),
+                        new Claim(JwtRegisteredClaimNames.Jti,Guid.NewGuid().ToString()),
+                        new Claim(JwtRegisteredClaimNames.UniqueName,user.UserName),
+
+
+                    };
+                    var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Tokens:Key"]));
+                    var cardentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+                    var token = new JwtSecurityToken(
+                        _config["Tokens:Issuer"],
+                        _config["Tokens:Audience"],
+                         claim,
+                        expires: DateTime.UtcNow.AddMinutes(20),
+                        signingCredentials: cardentials
+                        );
+
+                    var results = new
+                    {
+                        token = new JwtSecurityTokenHandler().WriteToken(token),
+                        expiration = token.ValidTo
+                    };
+                    return Created("", results);
+                }
+            }
+
+            return BadRequest();
         }
     }
 }
