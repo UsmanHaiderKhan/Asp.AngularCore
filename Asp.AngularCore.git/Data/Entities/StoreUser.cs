03cebea0c0fa030caacdbcfdb1d08f9ea88a2915
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-09-09 09:15:28 +0500

    Added New Entity To Store the User

---
+++ Asp.AngularCore.git/Data/Entities/StoreUser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Asp.AngularCore.git.Data.Entities
+{
+    public class StoreUser : IdentityUser
+    {
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+
+
+    }
+}
