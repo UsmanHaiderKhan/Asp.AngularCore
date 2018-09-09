388830f1b37dc041687d52816fcae5f00f26b263
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-09-09 09:13:41 +0500

    Update Get the Order Items By User Name or Authorize the Whole Controller

--- Asp.AngularCore.git/Controller/OrderItemsController.cs
+++ Asp.AngularCore.git/Controller/OrderItemsController.cs
@@ -2,6 +2,8 @@
 using Asp.AngularCore.git.Data.Entities;
 using Asp.AngularCore.git.ViewModel;
 using AutoMapper;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System.Collections.Generic;
@@ -10,6 +12,7 @@ using System.Linq;
 namespace Asp.AngularCore.git.Controller
 {
     [Route("/api/orders/{orderid}/items")]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
     public class OrderItemsController : Microsoft.AspNetCore.Mvc.Controller
     {
         private readonly ILKRepository _repository;
@@ -29,7 +32,7 @@ namespace Asp.AngularCore.git.Controller
         [HttpGet]
         public IActionResult Get(int orderId)
         {
-            var order = _repository.GetOrderById(orderId);
+            var order = _repository.GetOrderById(User.Identity.Name, orderId);
             if (order != null)
             {
                 return Ok(_mapper.Map<IEnumerable<OrderItem>, IEnumerable<OrderItemViewModel>>(order.Items));
@@ -42,7 +45,7 @@ namespace Asp.AngularCore.git.Controller
         public IActionResult Get(int orderId, int id)
         {
 
-            var order = _repository.GetOrderById(orderId);
+            var order = _repository.GetOrderById(User.Identity.Name, orderId);
             if (order != null)
             {
                 var item = order.Items.Where(o => o.Id == orderId).FirstOrDefault();
