dd00413706c1fc19783e40d0f6f92493fa88e0fe
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-09-09 09:17:42 +0500

    Update the Added Dummy User Info

--- Asp.AngularCore.git/Data/LkSeeds.cs
+++ Asp.AngularCore.git/Data/LkSeeds.cs
@@ -1,10 +1,12 @@
 ﻿using Asp.AngularCore.git.Data.Entities;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace Asp.AngularCore.git.Data
 {
@@ -12,15 +14,40 @@ namespace Asp.AngularCore.git.Data
     {
         private readonly LKContext _context;
         private readonly IHostingEnvironment _hosting;
-        public LkSeeds(LKContext context, IHostingEnvironment hosting)
+        private readonly UserManager<StoreUser> _user;
+
+
+        public LkSeeds(LKContext context, IHostingEnvironment hosting, UserManager<StoreUser> user)
         {
             _context = context;
             _hosting = hosting;
+            _user = user;
         }
 
-        public void Seed()
+        public async Task Seed()
         {
             _context.Database.EnsureCreated();
+
+            var userEmail = await _user.FindByEmailAsync("usmanhaiderkhan4@gmail.com");
+
+            if (userEmail == null)
+            {
+                userEmail = new StoreUser()
+                {
+                    FirstName = "Usman",
+                    LastName = "Khan",
+                    Email = "usmanhaiderkhan4@gmail.com",
+                    UserName = "Usman@hotmail.com"
+                };
+                var result = await _user.CreateAsync(userEmail, "P@ssw0rd!");
+                if (result != IdentityResult.Success)
+                {
+                    throw new InvalidOperationException("Failed To Add Default User");
+                }
+            }
+
+
+
             if (!_context.Products.Any())
             {
                 var file = Path.Combine(_hosting.ContentRootPath, "Data/art.json");
@@ -32,12 +59,14 @@ namespace Asp.AngularCore.git.Data
                 {
                     OrderDate = DateTime.Now,
                     OrderNumber = "12345",
+
                     Items = new List<OrderItem>()
                     {
                         new OrderItem()
                         {
                             Product = products.First(),
                             Quantity = 5,
+
                             UnitPrice = products.First().Price
                         }
                     }
